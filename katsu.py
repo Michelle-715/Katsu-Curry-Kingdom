0b2cebdc42440b7a481487eae5f6f2d712884cb8
Michelle-715 <michellesagredo1@gmail.com>
2025-07-02 16:32:42 +1000

    feat: extras to curry

--- katsu.py
+++ katsu.py
@@ -2,3 +2,6 @@ print("Welcome to Katsu Curry Kingdom!")
 
 size = input("What size of Katsu Curry would you like? (Small/Medium/Large): ")
 print(f"You chose a {size} Katsu Curry.")
+
+extras = input("Any extra ingredients?: ")
+print(f"Adding the following extras: {extras}")
